9721445cde4f091cbf5f5d718e8b453306eaefbd
Mwakai <mwakaimwambala@gmail.com>
2025-03-16 01:46:27 +0300

    feat: enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/Database";
 import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
